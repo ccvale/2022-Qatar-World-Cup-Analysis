d5170ff5d421f08e5ba424ac039622267487c997
Chris Valente <63901165+ccvale@users.noreply.github.com>
2023-04-29 02:48:28 -0400

    more

--- gui/app.py
+++ gui/app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 
 st.title('World Cup Matches')
@@ -24,7 +22,7 @@ def get_opponents(teamA):
 data_load_state = st.text('Loading data...')
 countries = load_data('gui/final_countries.csv')
 matches = load_data('gui/final_matches.csv')
-data_load_state.text('Data loaded!')
+data_load_state.text('')
 
 if st.checkbox('Show Groups'):
     groupID = st.select_slider('Select a Group', options=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
@@ -42,30 +40,39 @@ teamA = st.selectbox('Select country', countries['Nation'].unique())
 teamB = st.selectbox('Select opponent they\'ve played against', get_opponents(teamA))
 
 match = matches[((matches['team1'] == teamA) & (matches['team2'] == teamB)) | ((matches['team1'] == teamB) & (matches['team2'] == teamA))]
-st.write(match)
+st.write(match) # will be removed later
 
-stat = st.selectbox('Select statistic to visualize', options=['Possession', 'Goals', 'Assists', 'Shots Attempted', 'Total Passes', 'Fouls Committed', 'Forced Turnovers'])
+stat = st.selectbox('Select statistic to visualize', options=['Possession', 'Goals', 'Assists', 'Shots Attempted', 'Total Passes', 'Fouls Committed', 'Forced Turnovers', 'Goal Preventions'])
 st.subheader(f'Visualizing {stat} - {teamA} vs {teamB} ({match["category"].values[0]})')
 
 if stat == 'Possession':
     fig = px.pie(match, values=[match['possession team1'].values[0], match['possession team2'].values[0]], names=[match['team1'].values[0], match['team2'].values[0]])
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Goals':
-    # plotly express chart of goals scored by each team
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['number of goals team1'].values[0], match['number of goals team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Goals Scored')
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Assists':
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['assists team1'].values[0], match['assists team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Goals Assisted')
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Shots Attempted':
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['total attempts team1'].values[0], match['total attempts team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Shots Attempted')
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Total Passes':
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['passes completed team1'].values[0], match['passes completed team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Total Passes')
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Fouls Committed':
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['fouls against team1'].values[0], match['fouls against team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Fouls Committed')  
     st.plotly_chart(fig, use_container_width=True)
 elif stat == 'Forced Turnovers':
     fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['forced turnovers team1'].values[0], match['forced turnovers team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Forced Turnovers')
+    st.plotly_chart(fig, use_container_width=True)
+elif stat == 'Goal Preventions':
+    fig = px.bar(match, x=[match['team1'].values[0], match['team2'].values[0]], y=[match['goal preventions team1'].values[0], match['goal preventions team2'].values[0]], color=[match['team1'].values[0], match['team2'].values[0]])
+    fig.update_layout(xaxis_title='Team', yaxis_title='Goal Preventions')
     st.plotly_chart(fig, use_container_width=True)
